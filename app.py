50206884b5c0fbd077131f0574c764aace572e41
Matt Payne <pattmayne@gmail.com>
2025-10-23 18:48:40 -0400

    sending single image to the user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 import tarot_gen.tarot_gen as tgen
 
 app = Flask(__name__)
@@ -8,3 +8,9 @@ app = Flask(__name__)
 def hello_world():
     string = tgen.say_hi("Matt")
     return "<p>" + string + "</p>"
+
+@app.route("/img")
+def get_img():
+    img_bytes = tgen.get_img()
+    return send_file(img_bytes, mimetype='image/png')
+    #return "<h3>Getting IMG...</h3>"
--- tarot_gen/tarot_gen.py
+++ tarot_gen/tarot_gen.py
@@ -2,15 +2,20 @@
 
 from PIL import Image
 import os
+import io
 
 def say_hi(name):
-    bg_img = Image.open("tarot_gen/img/bg_01.png")
-    symbol_img = Image.open("tarot_gen/img/symbol_skull.png")
-    bg_img.paste(symbol_img, (0, 0), symbol_img)
-    bg_img.save('result.png')
 
 
     return_string = "Hi, " + name + "!"
     return return_string
 
+def get_img():
+    bg_img = Image.open("tarot_gen/img/bg_01.png")
+    symbol_img = Image.open("tarot_gen/img/symbol_skull.png")
+    bg_img.paste(symbol_img, (0, 0), symbol_img)
+    img_bytes = io.BytesIO() # THIS will be the image
+    bg_img.save(img_bytes, format='PNG')
+    img_bytes.seek(0) # reset stream position
+    return img_bytes
 
