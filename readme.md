4e4d739440fa80a85aab4574fb2626d392b3a481
Matt Payne <pattmayne@gmail.com>
2025-10-23 22:12:14 -0400

    random background now

---
+++ readme.md
@@ -0,0 +1,18 @@
+# NOTES
+
+# ELEMENTS OF EACH CARD
+## NORMAL CARD:
+* Background (landscape / setting)
+* Symbol
+* Number
+* POSSIBLY some kind of figure? OR NOT.
+* It's possible that we should have to tell a story by the conjunction of symbol, number, and landscape
+* BUT we want some kind of IDENTITY, or SITUATION, or FOIBLE / PATHOS
+## FACE CARD:
+* Background (landscape / setting)
+* LIMB-MONSTER (modular creature)
+* Maybe every card has a randomly-generated CHARACTER:
+* * head is symbol, torso with x number legs, arms and legs.
+* * Each limb has a story with it, or some attributes, some verbs and nouns to draw from
+* * We make a short phrase built from those limbs and the image chosen for each layer (BG too)
+
--- tarot_gen/tarot_gen.py
+++ tarot_gen/tarot_gen.py
@@ -3,6 +3,7 @@
 from PIL import Image
 import os
 import io
+import random
 
 def say_hi(name):
 
@@ -10,8 +11,23 @@ def say_hi(name):
     return_string = "Hi, " + name + "!"
     return return_string
 
+# TO DO: pick a 
+
+def get_bg():
+    file_names = [
+            "bg_01.png",
+            "bg_02.png",
+            "bg_03.png",
+            "bg_04.png" ]
+    random_index = random.randrange(0, len(file_names))
+    filename_prefix = "tarot_gen/img/"
+    full_filename = filename_prefix + file_names[random_index]
+    bg_img = Image.open(full_filename)
+
+    return bg_img
+
 def get_img():
-    bg_img = Image.open("tarot_gen/img/bg_01.png")
+    bg_img = get_bg() # Image.open("tarot_gen/img/bg_01.png")
     symbol_img = Image.open("tarot_gen/img/symbol_skull.png")
     bg_img.paste(symbol_img, (0, 0), symbol_img)
     img_bytes = io.BytesIO() # THIS will be the image
