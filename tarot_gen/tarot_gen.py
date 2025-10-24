640c2335075fb2f9ff9f4ad7f28107430a8cae62
Matt Payne <pattmayne@gmail.com>
2025-10-23 23:11:59 -0400

    resized symbols play across the screen

--- tarot_gen/tarot_gen.py
+++ tarot_gen/tarot_gen.py
@@ -6,8 +6,6 @@ import io
 import random
 
 def say_hi(name):
-
-
     return_string = "Hi, " + name + "!"
     return return_string
 
@@ -26,10 +24,48 @@ def get_bg():
 
     return bg_img
 
+
+# SYMBOLS are like SUITS, or identities
+# 
+
+def get_symbol():
+    file_names = [
+            "symbol_keyboard.png",
+            "symbol_pentacle.png",
+            "symbol_skull.png",
+            "symbol_gun.png",
+            "symbol_cup.png" ]
+    random_index = random.randrange(0, len(file_names))
+    filename_prefix = "tarot_gen/img/"
+    full_filename = filename_prefix + file_names[random_index]
+    return Image.open(full_filename)
+
+
 def get_img():
     bg_img = get_bg() # Image.open("tarot_gen/img/bg_01.png")
-    symbol_img = Image.open("tarot_gen/img/symbol_skull.png")
-    bg_img.paste(symbol_img, (0, 0), symbol_img)
+    symbol_img = get_symbol() # Image.open("tarot_gen/img/symbol_skull.png")
+
+    # We have the bg and symbol. Now find the correct size for the symbol
+    # must therefore find how many to draw
+
+    magnitude = random.randrange(1, 12)
+    img_size = bg_img.size
+    bg_width, bg_height = img_size
+    symbol_width = int(bg_width / 5)
+    symbol_img = symbol_img.resize((symbol_width, symbol_width))
+    
+    draw_y = 25
+    spaces = 0
+    for i in range(magnitude):
+        spaces = spaces + 1
+        if spaces > 4:
+            spaces = 0
+            draw_y = draw_y + symbol_width
+        draw_x = 25 + (spaces * symbol_width)
+        bg_img.paste(symbol_img, (draw_x, draw_y), symbol_img)
+
+    # we can have multiple algorithms for how to display the symbols.
+
     img_bytes = io.BytesIO() # THIS will be the image
     bg_img.save(img_bytes, format='PNG')
     img_bytes.seek(0) # reset stream position
