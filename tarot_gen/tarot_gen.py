7becc338ffea25359d767b72f1636b2dcf8490d5
Matt Payne <pattmayne@gmail.com>
2025-10-24 20:27:11 -0400

    odd numbers are working

--- tarot_gen/tarot_gen.py
+++ tarot_gen/tarot_gen.py
@@ -77,21 +77,61 @@ def get_row_sizes(magnitude):
     return list_of_row_sizes
 
 
+# @return int
+def get_distance_from_center(is_even, position, center):
+    if is_even:
+        position -= 1
+        center -= 1
+
+    diff = position - center
+    if diff < 0:
+        diff *= -1
+
+    print(" ")
+    print("DIFF:")
+    print(diff)
+    print(" ")
+
+    return diff
+
+
 # ALGORITHM for printing layers of arcs
 def print_symbols_arc(bg_img, symbol_img, magnitude):
     print("doing arc")
     symbol_width, symbol_height = symbol_img.size
     y_symbol_index = 0
+    arcing_y_adjustment = 0
     list_of_row_sizes = get_row_sizes(magnitude)
 
+    # this row of symbols to print, where the "row" is a value 
+    # which represents how many times to print symbol
     for symbols_in_this_row in list_of_row_sizes:
-        print(symbols_in_this_row)
+        #print(symbols_in_this_row)
 
-        offset = (SYMBOL_DIVISOR - symbols_in_this_row) * symbol_width / 2
+        # it must lower FIRST according to its distance from the center
+        # but that must be ENHANCED by being lower in the rows
+        # So START by getting the center - if it's even numbers, the first of the two center
+        center_index = (symbols_in_this_row - int(symbols_in_this_row / 2)) - 1
+
+        #arcing_y_adjustment = int(arcing_y_adjustment + (y_symbol_index * (symbol_width / 4)) * 1.5)
+        x_offset = (SYMBOL_DIVISOR - symbols_in_this_row) * symbol_width / 2
+        is_even = symbols_in_this_row % 2 == 0
+        quarter_symbol = int(symbol_width / 4)
+
+        is_string = " is even " if is_even else " is NOT even"
+        print("\nROW: center of ", symbols_in_this_row, " is ", center_index)
+        print(symbols_in_this_row, is_string)
 
         for symbol_index in range(symbols_in_this_row):
-            draw_x = int(offset + (symbol_index * symbol_width))
-            draw_y = int((symbol_width / 2) + (y_symbol_index * (symbol_width + 10)))
+            distance_from_center = get_distance_from_center(is_even, symbol_index + 1, center_index + 1)
+            print(symbol_index, " is ", distance_from_center, " from ", center_index)
+            # get
+            arcing_y_adjustment = distance_from_center * quarter_symbol
+                #0 if symbol_index == center_index || (is_even && symbol_index == center_index + 1) else
+
+            draw_x = int(x_offset + (symbol_index * symbol_width))
+            draw_y = int((symbol_width / 2) + (y_symbol_index * (symbol_width + 10))) + arcing_y_adjustment
+            print('arcing y:', arcing_y_adjustment)
             # finally actually draw the image
             bg_img.paste(symbol_img, (draw_x, draw_y), symbol_img)
 
@@ -111,13 +151,13 @@ def print_symbols(bg_img, symbol_img, magnitude):
 
     pattern_enums = list(SymbolPatterns)
     pattern_enum = pattern_enums[int(random.randrange(0, len(pattern_enums)))]
-
-
     
     if pattern_enum == SymbolPatterns.ARC:
         print_symbols_arc(bg_img, symbol_img, magnitude)
     elif pattern_enum == SymbolPatterns.FLAT:
-        print_symbols_flat(bg_img, symbol_img, magnitude)
+        #print_symbols_flat(bg_img, symbol_img, magnitude)
+        print_symbols_arc(bg_img, symbol_img, magnitude)
+
 
 
 
@@ -143,7 +183,8 @@ def print_symbols_flat(bg_img, symbol_img, magnitude):
 
 
 
-
+# The main function to get an image
+# @returns raw image bytes
 def get_img():
     bg_img = get_bg() # Image.open("tarot_gen/img/bg_01.png")
     symbol_img = get_symbol() # Image.open("tarot_gen/img/symbol_skull.png")
